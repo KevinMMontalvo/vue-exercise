f26e491d39ae865064a3a0c18b5e2577a66f08b5
Kevin Montalvo V <montalvokevin22@gmail.com>
2021-08-29 20:23:22 -0500

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<v-container>
+		<v-form ref="form">
+			<v-row class="text-center">
+				<v-col cols="6">
+					<!--Textfield del nombre-->
+				</v-col>
+				<v-col cols="6">
+					<!--Textfield del apellido-->
+				</v-col>
+				<v-col cols="12">
+					<!--Boton-->
+				</v-col>
+			</v-row>
+			<v-snackbar v-model="message.visible" :color="message.color">{{ message.text }}</v-snackbar>
+		</v-form>
+	</v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from "axios";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+	name: 'HelloWorld',
+
+	data: () => ({
+		name: '',
+		surname: '',
+		message: {
+			visible: false,
+			color: '',
+			text: ''
+		}
+	}),
+	methods: {
+		save()
+		{
+			let data = this.$data;
+
+			let item = {
+				username: this.name,
+				email: this.surname,
+				password: 'pass'
+			};
+
+			axios.post('https://autorepuestos-dev.herokuapp.com/signup', item).then(function (response)
+			{
+				data.message = {
+					visible: true,
+					text: 'Usuario creado',
+					color: 'success'
+				};
+				data.name = '';
+				data.surname = '';
+			}).catch(function (error)
+			{
+				console.error(error);
+				data.message = {
+					visible: true,
+					text: 'Hubo un error al crear el usuario',
+					color: 'error'
+				};
+			}).then(function ()
+			{
+			});
+		}
+	}
+};
+</script>
